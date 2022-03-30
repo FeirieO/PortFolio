6c1a7104427c96b35dd7766aa3554f244994ae05
feirie001@gmail.com <feirie001@gmail.com>
2022-03-31 00:50:27 +0100

    credentials

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,22 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
+
+
+function toggleskills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills_current skills_close'
+    }
+    if(itemClass === 'skills_content skills_close'){
+        this.parentNode.className = 'skills_content skills_open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleskills)
+})
